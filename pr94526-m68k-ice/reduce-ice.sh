eb9191b162b6e94325430780f85546c8876222be
Martin Liska <mliska@suse.cz>
2020-04-10 12:36:32 +0200

    Fix up path.

--- pr94526-m68k-ice/reduce-ice.sh
+++ pr94526-m68k-ice/reduce-ice.sh
@@ -2,4 +2,4 @@
 
 TC1=${1:-pr94526.i}
 
-./cc1 -fpreprocessed -O2 -g $TC1 -o /dev/stdout 2>&1 | grep 'new_elt_loc_list'
+/home/marxin/Programming/creduce-benchmarks/pr94526-m68k-ice/cc1 -fpreprocessed -O2 -g $TC1 -o /dev/stdout 2>&1 | grep 'new_elt_loc_list'
