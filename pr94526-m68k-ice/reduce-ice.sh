c17b73c66033c47793a0d03dd7c781e293ac178b
Martin Liska <mliska@suse.cz>
2020-04-17 11:41:10 +0200

    Use better names for test-cases.

--- pr94526-m68k-ice/reduce-ice.sh
+++ pr94526-m68k-ice/reduce-ice.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TC1=${1:-pr94526.i}
+TC1=${1:-ice.i}
 
 /home/marxin/Programming/creduce-benchmarks/pr94526-m68k-ice/cc1 -fpreprocessed -quiet -O2 -g $TC1 -o /dev/stdout 2>&1 | grep 'new_elt_loc_list'
